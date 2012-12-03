b1bddddcc82da463b00518bda11db1355bf22d74
mihoover <mattmihoover@gmail.com>
2012-12-02 18:00:19 -0700

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public HelloWorld(){
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	}
+
+}
